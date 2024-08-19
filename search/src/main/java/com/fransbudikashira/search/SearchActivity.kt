584414261e59d105a8975e0ef5ce5ed82781f9bc
Frans Budi Kashira <fransbudikashira@gmail.com>
2024-08-19 21:28:24 +0700

    update terbaru

---
+++ search/src/main/java/com/fransbudikashira/search/SearchActivity.kt
@@ -0,0 +1,20 @@
+package com.fransbudikashira.search
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SearchScreen(
+                onClick = {}
+            )
+        }
+    }
+}
